d7e00996d8b6fbea4f6362713814fe11016e991e
arpitpandey <arpitpandey1181@gmail.com>
2023-10-05 17:47:26 +0530

    uplode program

---
+++ Looping Java/src/pattern/dimond/DimondPattern02.java
@@ -0,0 +1,36 @@
+package pattern.dimond;
+// 1 2 3 4 5 6 
+//  2 3 4 5 6 
+//   3 4 5 6 
+//    4 5 6 
+//     5 6 
+//      6 
+//     5 6 
+//    4 5 6 
+//   3 4 5 6 
+//  2 3 4 5 6 
+// 1 2 3 4 5 6 
+
+public class DimondPattern02 {
+    public static void main(String[] args) {
+      int n = 6;
+      for(int i = 1; i <= n;i++){
+          for(int j = 1; j < i; j++){
+            System.out.print(" ");
+           }
+          for(int j = i; j <= n; j++){
+            System.out.print(j+" ");
+          }
+          System.out.println();
+        }
+        for(int i = 1; i <= n-1; i++){
+            for(int j = n-1; j > i; j--){
+                System.out.print(" ");
+            }
+            for(int j = n-i; j <= n; j++ ){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }   
+    }
+}
