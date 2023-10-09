7568909e61fb5227a1b0b9f838095e0eee2105d8
akashydv311 <akashydv311@gmail.com>
2023-10-10 01:57:38 +0530

    java var, datatype. bit byte

---
+++ Java Introduction/src/basicsjava/Demo01.java
@@ -0,0 +1,42 @@
+package basicsjava;
+
+public class Demo01 {
+
+    static String name = "INDIA";
+
+    public static void main(String[] args) {
+
+        int x = 10;
+
+        if (true) {
+            int num1 = 100;
+            System.out.println(num1);
+        } else {
+            // System.out.println(num1);
+            System.out.println(x);
+        }
+
+    }
+
+}
+
+/*
+ * 
+ * if(){
+ * body if ( block of if)
+ * }else{
+ * body else ( block if else)
+ * }
+ * 
+ * 
+ * for(; ; ){
+ * 
+ * body for ( block of for)
+ * 
+ * }
+ * 
+ * for(int i=1; ;){
+ * sysout(i);
+ * }
+ * sysout(i);
+ */
