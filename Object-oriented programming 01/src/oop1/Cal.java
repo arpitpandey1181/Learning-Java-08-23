e126f0f2bb418397993bf3a13a13dd43ede907e9
akashydv311 <akashydv311@gmail.com>
2023-10-20 09:25:20 -0700

    code updated

---
+++ Object-oriented programming 01/src/oop1/Cal.java
@@ -0,0 +1,22 @@
+package oop1;
+
+class Calculatorx {
+    // props
+
+    // method should add 2 numbers and return double
+
+    // <return type> <method name> (){
+    // // body
+    // }
+
+    double add() {
+        return 89 + 89;
+    }
+
+}
+
+public class Cal {
+    public static void main(String[] args) {
+
+    }
+}
