28e12273dd81d853fe565e211bf1b8c11ef2c886
akashydv311 <akashydv311@gmail.com>
2023-10-21 22:17:39 -0700

    assignment added

---
+++ Object-oriented programming 01/src/methods/assignment/Question04.java
@@ -0,0 +1,13 @@
+
+package methods.assignment;
+
+class Soluction04 {
+    // take a input from user and calculate it's factorial if the number is prime
+    // number Here you have to create 2 methods one is factorial and other is prime
+}
+
+public class Question04 {
+    public static void main(String[] args) {
+
+    }
+}
