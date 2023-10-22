bcd4a21b06e4836f6f1d5d2e092d5f84954933f1
akashydv311 <akashydv311@gmail.com>
2023-10-21 21:20:14 -0700

    assignments addd -> method

---
+++ Object-oriented programming 01/src/methods/assignment/Question01.java
@@ -0,0 +1,18 @@
+package methods.assignment;
+
+class Soluction01 {
+    // Write a Java method called isPrime that takes an integer as input and returns
+    // a boolean value indicating whether the input number is a prime number or not
+
+    // Write a Java method called calculateFactorial that takes a non-negative
+    // integer as input and returns its factorial. The factorial of a non-negative
+    // integer N, denoted as N!, is the product of all positive integers less than
+    // or equal to N.
+
+}
+
+public class Question01 {
+    public static void main(String[] args) {
+
+    }
+}
