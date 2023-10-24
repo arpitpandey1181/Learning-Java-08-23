96fd8f5d7ff46698343c98d4527c2119fac49374
Akash Yadav <133625194+akashydv311@users.noreply.github.com>
2023-10-24 19:20:40 +0530

    Update Question04.java

--- Object-oriented programming 01/src/methods/assignment/Question04.java
+++ Object-oriented programming 01/src/methods/assignment/Question04.java
@@ -5,30 +5,40 @@ import java.util.Scanner;;
 class Soluction04 {
     // take a input from user and calculate it's factorial if the number is prime
     // number Here you have to create 2 methods one is factorial and other is prime
-    int num;
-    void factorial(int num){
-        int ans = 1;
-        for(int i = 1; i <= num; i++){
-            ans *= i;
-       }
-       System.out.println(ans);
-    }
-    void prime(int num){
-        if(num%2==0){
-            System.out.println("it is prime number");
-        }
-        else{
-            System.out.println("it is not prime number");
+    boolean prime(int num){
+        if (num > 0) {
+            // number should be divided only with itself.
+            for (int i = 2; i < num; i++) {
+                if (num % i == 0) {
+                    return false;
+                }
+            }
+            return true;
+        } else {
+            return false;
         }
     }
+    int factorial(int num){
+        if(prime(num)){
+            int ans = 1;
+            for(int i = 1; i <= num; i++){
+               ans *= i;
+            }
+        return ans;
+      }else{
+            return -1;
+        }
 }
+    
 public class Question04 {
     public static void main(String[] args) {
          Scanner sc = new Scanner(System .in);
          System.out.println("Enter number : ");
          int n = sc.nextInt();
-         Soluction04 fac = new Soluction04();
-         fac.factorial(n);
-         fac.prime(n);
+         sc.close();
+        
+         Soluction04 sol4 = new Soluction04();
+         int fact = sol4.factorial(n);
+         System.out.println("Factorial is: "+ fact);
     }
 }
