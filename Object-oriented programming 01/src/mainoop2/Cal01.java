a7004d1995edc085feef928b6ba2e2dbc8634c59
akashydv311 <akashydv311@gmail.com>
2023-11-27 20:00:12 +0530

    interface example

---
+++ Object-oriented programming 01/src/mainoop2/Cal01.java
@@ -0,0 +1,26 @@
+package mainoop2;
+
+interface Calculator {
+
+    void takeInput(int num1, int num2);
+
+    int add();
+
+    int sub();
+
+    int muli();
+}
+
+class Calsy {
+
+    @Override
+    public void takeInput(int num1, int num2) {
+
+    }
+}
+
+// public class Cal01 {
+// public static void main(String[] args) {
+
+// }
+// }
