0ba5286335b51ea1abffd27dd136b1a2547e90c5
akashydv311 <akashydv311@gmail.com>
2023-11-27 19:31:58 +0530

    exmaple of inheritance

---
+++ Object-oriented programming 01/src/mainoop2/Interface01.java
@@ -0,0 +1,13 @@
+package mainoop2;
+
+interface Animal {
+    void makeSound(); // Abstract method (method without a body)
+
+    void eat();
+}
+
+public class Interface01 {
+    public static void main(String[] args) {
+
+    }
+}
