3dd51303785d2dacf7a5528c3f238b409b1d028a
akashydv311 <akashydv311@gmail.com>
2023-11-27 20:10:19 +0530

    calculator by interface

--- Object-oriented programming 01/src/mainoop2/Cal01.java
+++ Object-oriented programming 01/src/mainoop2/Cal01.java
@@ -2,7 +2,7 @@ package mainoop2;
 
 interface Calculator {
 
-    void takeInput(int num1, int num2);
+    void takeInput();
 
     int add();
 
@@ -11,16 +11,40 @@ interface Calculator {
     int muli();
 }
 
-class Calsy {
+class Calsy implements Calculator {
+    private int num1;
+    private int num2;
 
     @Override
-    public void takeInput(int num1, int num2) {
+    public void takeInput() {
+        this.num1 = 34;
+        this.num2 = 89;
+    }
 
+    @Override
+    public int add() {
+        return num1 + num2;
+    }
+
+    @Override
+    public int sub() {
+        return num1 - num2;
+    }
+
+    @Override
+    public int muli() {
+        return num1 * num2;
     }
 }
 
-// public class Cal01 {
-// public static void main(String[] args) {
+public class Cal01 {
+    public static void main(String[] args) {
 
-// }
-// }
+        Calsy cal = new Calsy();
+
+        cal.takeInput();
+        System.out.println(cal.add());
+        System.out.println(cal.sub());
+        System.out.println(cal.muli());
+    }
+}
