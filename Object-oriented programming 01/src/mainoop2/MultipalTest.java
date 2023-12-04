45aa94c1265e4e5aaafbf62165cfc6b29aa48ef0
akashydv311 <akashydv311@gmail.com>
2023-12-04 20:06:26 +0530

    inheritance example done

---
+++ Object-oriented programming 01/src/mainoop2/MultipalTest.java
@@ -0,0 +1,19 @@
+package mainoop2;
+
+class Father {
+
+}
+
+interface Mother {
+
+}
+
+class Child extends Father implements Mother {
+
+}
+
+public class MultipalTest {
+    public static void main(String[] args) {
+
+    }
+}
