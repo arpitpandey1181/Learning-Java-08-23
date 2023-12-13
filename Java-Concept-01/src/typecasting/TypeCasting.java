739af7e8ef6e29f1347a8648d531aecbce96e0c7
akashydv311 <akashydv311@gmail.com>
2023-12-14 00:31:21 +0530

    java concept folder added

---
+++ Java-Concept-01/src/typecasting/TypeCasting.java
@@ -0,0 +1,16 @@
+package typecasting;
+
+public class TypeCasting {
+    public static void main(String[] args) {
+
+        // type casting
+        int x = 10;
+        float y = 20.7f;
+
+        // float ans = x + y;
+
+        int ans = x + (int) y;
+
+        System.out.println(ans); // type casting
+    }
+}
---
+++ Java-Concept-01/src/typecasting/TypeConversion.java
@@ -0,0 +1,17 @@
+package typecasting;
+
+public class TypeConversion {
+    public static void main(String[] args) {
+
+        // type conversion
+        int num1 = 10;
+        double num2 = 45;
+
+        double ans = num1 + num2;// automatic type conversion
+
+        System.out.println(ans);
+
+        // System.out.println(num1);
+
+    }
+}
