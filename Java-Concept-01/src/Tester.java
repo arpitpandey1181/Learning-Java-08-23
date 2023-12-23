914015ebae0a0a750ab31fd1d5468097fbd4aec8
akashydv311 <akashydv311@gmail.com>
2023-12-23 23:26:41 +0530

    Tester.java modified

--- Java-Concept-01/src/Tester.java
+++ Java-Concept-01/src/Tester.java
@@ -1,41 +1,6 @@
 
-abstract class Car1 {
-
-    public abstract void engine();
-
-    public abstract void drive();
-
-    public void carColor() {
-        System.out.println("Color is green!");
-    }
-}
-
-// concrete class
-class Suzuki extends Car1 {
-
-    public void engine() {
-        System.out.println("Engine is working!");
-    }
-
-    public void drive() {
-        System.out.println("Drive your car!");
-    }
-
-    public void run() {
-        System.out.println("CAn't run");
-    }
-
-}
-
 public class Tester {
     public static void main(String[] args) {
 
-        Car1 mycar;
-        mycar = new Suzuki();
-
-        mycar.carColor();
-        mycar.engine();
-        mycar.drive();
-        // mycar.run();
     }
 }
