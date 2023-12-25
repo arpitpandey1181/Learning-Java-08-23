d3e49d44a7b4d3b3007b2ab5733b84a42e250839
akashydv311 <akashydv311@gmail.com>
2023-12-25 22:17:49 +0530

    abstract example

---
+++ Java-Concept-01/src/abstractclass/Revision.java
@@ -0,0 +1,34 @@
+package abstractclass;
+
+abstract class MobApp {
+
+    public void ui() {
+
+    }
+
+    public void design() {
+
+    }
+
+    public abstract void code();
+
+    public abstract void testing();
+
+}
+
+class Implimantation extends MobApp {
+
+    @Override
+    public void code() {
+    }
+
+    @Override
+    public void testing() {
+    }
+}
+
+public class Revision {
+    public static void main(String[] args) {
+
+    }
+}
